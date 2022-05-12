c1745bd094bbff2ca0cc5cbf0e165beeb28b7255
Adrian Połubiński <polubis@wp.pl>
2022-05-12 17:11:25 +0200

    Improvements

--- src/snippets/notPureFunction.ts
+++ src/snippets/notPureFunction.ts
@@ -16,11 +16,10 @@ export const removeObjectProperty = <O extends Obj, K extends keyof O>(
 const initObject = { 0: "Piotr", 1: "Jacob" };
 
 // That's not pure function. We changing object
-// from outer cope after function call.
+// from outer scope after function call.
 const ob = removeObjectProperty(initObject, 1);
 
-// Also after second call the result
-// of function is different - function is not predictable.
+// Also after second call the result of function is different - function is not predictable.
 
 console.log(initObject, ob); // { 0: 'Piotr' }, { 0: 'Piotr' }
 
