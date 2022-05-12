0a950a598a33fe37ea226d61949c088d0a23caa2
Adrian Połubiński <polubis@wp.pl>
2022-05-12 16:42:41 +0200

    Add ramda

--- src/snippets/currying.ts
+++ src/snippets/currying.ts
@@ -1,7 +1,12 @@
+import { curry } from "ramda";
+
 export const sum = (a: number, b: number): number => a + b;
 
 // currying
 export const curriedSum = (a: number) => (b: number) => a + b;
 
+// auto currying
+export const autoCurriedSum = curry(sum);
+
 // Transformation from fn(a, b) to fn(a)(b) that's all
-console.log(sum(3, 4), curriedSum(3)(4));
+console.log(sum(3, 4), curriedSum(3)(4), autoCurriedSum(3)(4));
