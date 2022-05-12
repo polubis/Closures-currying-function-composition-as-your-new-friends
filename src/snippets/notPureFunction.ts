ed277bc69ac082e630cabcf1f4020149322fcaf2
Adrian Połubiński <polubis@wp.pl>
2022-05-12 14:45:25 +0200

    Add notPureFunction example

---
+++ src/snippets/notPureFunction.ts
@@ -0,0 +1,24 @@
+type Prop = string | number;
+
+type Obj = Record<Prop, unknown>;
+
+export const removeObjectProperty = <O extends Obj, K extends keyof O>(
+  obj: O,
+  key: K
+): Omit<O, K> => {
+  delete obj[key];
+  return obj;
+};
+
+const initObject = { 0: "Piotr", 1: "Jacob" };
+
+// That's not pure function. We changing object
+// from outer cope after function call.
+const ob = removeObjectProperty(initObject, 1);
+
+// Also after second call the result
+// of function is different - function is not predictable.
+
+console.log(initObject, ob); // { 0: 'Piotr' }, { 0: 'Piotr' }
+
+removeObjectProperty(initObject, 0); // {}, {};
